bbb342830266f6602550eaa7fc5f2301d02cef56
Adam <adam.milko@gmail.com>
2021-02-13 14:40:25 +0100

    Added basic structure and styles

--- task-y21-m02/src/app/app.component.ts
+++ task-y21-m02/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'task-y21-m02';
+
+  public showPlaylist(){
+    
+  }
 }
--- task-y21-m02/src/app/app.module.ts
+++ task-y21-m02/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PlaylistComponent } from './playlist/playlist.component';
+import { SearchComponent } from './search/search.component';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaylistComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]

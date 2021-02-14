81c0c0dd6258d39b8f346ce7a785d5b115695876
Adam <adam.milko@gmail.com>
2021-02-14 18:41:54 +0100

    save playlist feature works -> exports required data to txt file

--- task-y21-m02/src/app/save-modal/save-modal.component.ts
+++ task-y21-m02/src/app/save-modal/save-modal.component.ts
@@ -1,17 +1,23 @@
-import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, Inject, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { saveAs } from 'file-saver';
-import { Subscription } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 import { FormBuilder } from '@angular/forms';
+import { PlaylistItem } from '../services/search.service';
+
+interface exportedFileData {
+  name: string,
+  email: string,
+  playlist: PlaylistItem[]
+}
 
 @Component({
   selector: 'app-save-modal',
   templateUrl: './save-modal.component.html',
   styleUrls: ['./save-modal.component.scss']
 })
-export class SaveModalComponent implements OnInit, OnDestroy {
+export class SaveModalComponent implements OnInit {
 
   public savePlaylistForm = this.formBuilder.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
@@ -21,63 +27,31 @@ export class SaveModalComponent implements OnInit, OnDestroy {
   get name() { return this.savePlaylistForm.get('name'); }
   get email() { return this.savePlaylistForm.get('email'); }
 
-  private formSubsc: Subscription = Subscription.EMPTY;
-
   constructor(
     public dialogRef: MatDialogRef<SaveModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: { playlist$: any },
     private formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
-    // this.subscribeToFormValue();
-    console.log('THIS FORM', this.savePlaylistForm)
-  }
+  ngOnInit(): void { }
 
-  savePlaylist() {
-    console.log('FORM VLUE',this.savePlaylistForm.value)
-    let playlistJsonData='';
+  savePlaylist(): void {
+    let playlistJsonData = '';
     this.data.playlist$.pipe(
       debounceTime(1500)
     )
-    .subscribe(
-      (playlist: any) => {
-        playlistJsonData = JSON.stringify(playlist);
-        console.log('jsonData', playlistJsonData)
-        const blob = new Blob([playlistJsonData], { type: "text/plain;charset=utf-8" });
-        // saveAs(blob, "playlist.txt");
-        this.dialogRef.close();
-      }
-    )
-  }
-
-  // private subscribeToFormValue() {
-  //   this.formSubsc = this.savePlaylistForm.get('searchValue')!.valueChanges.pipe(
-  //     debounceTime(1000), // to prevent too many requests on fast typing input
-  //     mergeMap(
-  //       (formValue: string) => {
-  //         this.isSearchLoading = true;
-  //         this.currentSearchIndex = 0;
-  //         this.loadedItems = [];
-  //         this.playlistService.clearSearchResults();
-  //         this.currentSearchQuery = formValue;
-  //         return this.searchService.getData(formValue, this.currentSearchIndex);
-  //       }),
-  //       debounceTime(750), // to pretend longer responce in order to see loading feature better
-  //   )
-  //     .subscribe(
-  //       (searchResult: SearchResult) => {
-  //         console.log('value of search', searchResult)
-  //         this.loadedItems = searchResult.data;
-  //         for (let singleResult of searchResult.data) {
-  //           this.playlistService.addToSearchResults(singleResult);
-  //         }
-  //         this.isSearchLoading = false;
-  //       },
-  //       error => console.log('TU OBSŁUGA MOJEGO BŁĘDU', error)
-  //     )
-  // }
-
-  ngOnDestroy(): void{
-    this.formSubsc.unsubscribe();
+      .subscribe(
+        (playlist: PlaylistItem[]) => {
+          playlistJsonData = JSON.stringify(playlist);
+          const exportedFileData: exportedFileData = {
+            name: this.savePlaylistForm.get('name')!.value,
+            email: this.savePlaylistForm.get('email')!.value,
+            playlist: playlist
+          };
+          const exportedFileDataJson = JSON.stringify(exportedFileData);
+          const blob = new Blob([exportedFileDataJson], { type: "text/plain;charset=utf-8" });
+          saveAs(blob, exportedFileData.name + ".txt");
+          this.dialogRef.close();
+        }
+      )
   }
 }
